3b11195015f7acaad5ded8b997a9188035dbf9f3
Ruslan Symonenko <symonenkors@gmail.com>
2023-03-24 21:47:20 +0200

    added functionality of changing forms by stage

--- main.js
+++ main.js
@@ -6,27 +6,47 @@ document.addEventListener('DOMContentLoaded', () => {
   let currentStage = 1;
 
   function setProgressBar (stage, stagesElement) {
+    const formsContainer = document.querySelector('.app-form');
+
     switch (stage) {
       case 1:
         progressBar1.style.width = '0%';
         progressBar2.style.width = '0%';
         changeStagesColor(stagesElement, currentStage);
+        formsContainer.style.transform = 'translateX(-0%)';
         break
       case 2:
         progressBar1.style.width = '100%';
         progressBar2.style.width = '0%';
         changeStagesColor(stagesElement, currentStage);
+        formsContainer.style.transform = 'translateX(-100%)';
         break
       case 3:
         progressBar1.style.width = '100%';
         progressBar2.style.width = '100%';
         changeStagesColor(stagesElement, currentStage);
+        formsContainer.style.transform = 'translateX(-200%)';
         break
       default:
         progressBar1.style.width = '0%';
         progressBar2.style.width = '0%';
         changeStagesColor(stagesElement, currentStage);
+        formsContainer.style.transform = 'translateX(0%)';
     }
+  }
+
+  function setFormVisibility (stage) { 
+    const forms = document.querySelectorAll('.app-form__display');
+
+    forms.forEach(element => {
+      let elementIndex = element.getAttribute('data-formIndex');
+
+      if(elementIndex == stage) {
+        element.classList.add('app-form__display--active');
+      } else {
+        element.classList.remove('app-form__display--active');
+      }
+    })
   }
 
   function changeStagesColor (elements, currentIndex) {
@@ -41,12 +61,14 @@ document.addEventListener('DOMContentLoaded', () => {
     stages.forEach((stage, index) => {
       stage.addEventListener('click', (e) => {
         currentStage = +e.target.textContent;
+
         setProgressBar(currentStage, stages);
+        setFormVisibility(currentStage);
       })
     })
   }
 
   setProgressBar(currentStage, stages);
-
   changeProgressByStage();
+  setFormVisibility(currentStage);
 })
